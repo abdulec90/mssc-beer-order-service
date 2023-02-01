cfcf4fb9634027eda615cfaecc1a0779e07c6b16
abdulec90 <abdulec90@gmail.com>
2023-02-01 17:32:38 +0530

    updating service port

--- src/main/java/guru/sfg/beer/order/service/domain/Customer.java
+++ src/main/java/guru/sfg/beer/order/service/domain/Customer.java
@@ -21,9 +21,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.sql.Timestamp;
 import java.util.Set;
 import java.util.UUID;
--- src/main/java/guru/sfg/beer/order/service/web/mappers/BeerOrderMapper.java
+++ src/main/java/guru/sfg/beer/order/service/web/mappers/BeerOrderMapper.java
@@ -20,8 +20,10 @@ package guru.sfg.beer.order.service.web.mappers;
 import guru.sfg.beer.order.service.domain.BeerOrder;
 import guru.sfg.beer.order.service.web.model.BeerOrderDto;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
 @Mapper(uses = {DateMapper.class, BeerOrderLineMapper.class})
+@Component
 public interface BeerOrderMapper {
 
     BeerOrderDto beerOrderToDto(BeerOrder beerOrder);
